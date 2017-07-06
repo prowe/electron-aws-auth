d045e77b3ced4f077ad233bb86cfa862c0b5ed0b
Paul Rowe <prowe@sourceallies.com>
2017-07-05 21:33:53 -0500

    fixing bug with set -e

--- login-aws-dev.sh
+++ login-aws-dev.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-set -e
-
-exportVars=$(\
+source /dev/stdin <<<"$(\
     GOOGLE_CLIENT_ID='623591274072-kvu1ue0tq9oabke17r80itgpbam81i5f.apps.googleusercontent.com' \
     ROLE_ARN='arn:aws:iam::729161019481:role/Observer' \
-    npm --silent start)
-
-source /dev/stdin <<<"$exportVars"
+    npm --silent start)"
