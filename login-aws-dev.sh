bd5e115b8d555f78602d6e7c0118e6cb12ab1a9c
Ajinkya <akulkarni@sourceallies.com>
2017-07-11 14:23:22 -0500

    Google Client Id from Matt's G Suite Developer account.  Identity pool from the SAI AWS DEV Account

--- login-aws-dev.sh
+++ login-aws-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source /dev/stdin <<<"$(\
-    GOOGLE_CLIENT_ID='623591274072-kvu1ue0tq9oabke17r80itgpbam81i5f.apps.googleusercontent.com' \
-    IDENTITY_POOL_ID='us-east-1:a37629b5-3b97-4570-8f77-cd4d6b8b8072' \
+    GOOGLE_CLIENT_ID='131506248849-73u9trpcni7vvdarbug93sb2r9hnn3fj.apps.googleusercontent.com' \
+    IDENTITY_POOL_ID='us-east-1:22c51ffa-1102-4be6-b373-210be1af71e9' \
     npm --silent start)"
