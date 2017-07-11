58294e6b32e8f2fa374af7f4c9ca082455910cea
Paul Rowe <prowe@sourceallies.com>
2017-07-10 20:37:22 -0500

    clouformation template is working

--- login-aws-dev.sh
+++ login-aws-dev.sh
@@ -2,5 +2,5 @@
 
 source /dev/stdin <<<"$(\
     GOOGLE_CLIENT_ID='623591274072-kvu1ue0tq9oabke17r80itgpbam81i5f.apps.googleusercontent.com' \
-    IDENTITY_POOL_ID='us-east-1:76f0e395-cfda-4c92-93ba-47dac3101ac6' \
+    IDENTITY_POOL_ID='us-east-1:a37629b5-3b97-4570-8f77-cd4d6b8b8072' \
     npm --silent start)"
